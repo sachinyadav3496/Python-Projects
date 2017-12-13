e5fcc5096fa4c2596f2da5670faef1eafbddedaa
sachin <sachinyadav3496@gmail.com>
2017-12-13 09:17:35 +0530

    link all pages with header.html to inherit the property:wq

--- event/event/urls.py
+++ event/event/urls.py
@@ -17,7 +17,8 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('/',"myapp.urls",name='main')
+
     path('admin/', admin.site.urls),
     path('',include("myapp.urls"),name='main'),
+     path('/',include("myapp.urls"),name='main'),
 ]
