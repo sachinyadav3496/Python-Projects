3937546369579cf43b5ff281a980c7219ca9220e
Sachin Yadav <sachinyadav3496@gmail.com>
2017-12-13 09:21:00 +0530

    Update urls.py

--- event/event/urls.py
+++ event/event/urls.py
@@ -20,5 +20,5 @@ urlpatterns = [
 
     path('admin/', admin.site.urls),
     path('',include("myapp.urls"),name='main'),
-     path('/',include("myapp.urls"),name='main'),
+     path('',include("myapp.urls"),name='main'),
 ]
