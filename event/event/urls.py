af7b40fe8f0292ac9125dc108855a8aa32b26dff
sachin <sachinyadav3496@gmail.com>
2017-12-13 06:45:24 +0530

    added default home url
    Just added a new url to launch project directly as localhost
    
    rather then localhost/home

--- event/event/urls.py
+++ event/event/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
+    path('/',"myapp.urls",name='main')
     path('admin/', admin.site.urls),
     path('',include("myapp.urls"),name='main'),
 ]
